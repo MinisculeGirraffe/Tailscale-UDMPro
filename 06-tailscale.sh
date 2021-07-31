23cbf8d1258be1664f0844765e15c6217f3e5a21
Daniel Norred <daniel@nor.red>
2021-07-31 10:55:58 -0500

    Script IPv6 Updates

--- 06-tailscale.sh
+++ 06-tailscale.sh
@@ -3,8 +3,8 @@
 VLAN=0
 IPV4_IP="10.1.10.2"
 IPV4_GW="10.1.10.1/24"
-IPV6_IP="2600:1700:eec0:7310::2"
-IPV6_GW="2600:1700:eec0:7310::1/64"
+IPV6_IP="2603:8080:7002:3600::2"
+IPV6_GW="2603:8080:7002:3600::1/64"
 
 # set VLAN bridge promiscuous
 ip link set br${VLAN} promisc on
@@ -23,16 +23,20 @@ ip link set br${VLAN}.mac up
 ip route add ${IPV4_IP}/32 dev br${VLAN}.mac
 ip -6 route add ${IPV6_IP}/128 dev br${VLAN}.mac
 
-ip -6 route add fd7a:115c:a1e0:ab12:4843::/80 via ${IPV6_IP}
+
 
 # Add Tailscale IP Routes via Container
-ip route add 100.64.0.0/10 via ${IPV4_IP} dev br${VLAN}.macip -6 route add 2600:1f16:116:3b00::/56 via ${IPV6_IP}
+ip route add 100.64.0.0/10 via ${IPV4_IP} dev br${VLAN}.mac
+# Change, your link local ipv6 is likely different
+ip -6 route add fd7a:115c:a1e0:ab12:4843::/80 via ${IPV6_IP}
 
 #Add routes from other subnet routers
 ip route add 10.2.0.0/16 via ${IPV4_IP}
-
+ip -6 route add 2600:1f16:116:3b00::/56 via ${IPV6_IP}
 #start container & tailscale 
 docker start tailscaled
-docker exec tailscaled tailscale up --hostname=UDM-Pro --accept-routes=true --advertise-routes=10.1.0.0/16,2600:1700:eec0:7310::/60
+docker exec tailscaled sysctl -w net.ipv6.conf.all.forwarding=1
+docker exec tailscaled tailscale up --hostname=UDM-Pro --accept-routes=true --advertise-routes=10.1.0.0/16,2603:8080:7002:3600::/56
+
 
 
