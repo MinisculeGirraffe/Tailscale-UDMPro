a7267cd00be910ca84781c4daa2eaedbaccc9a75
Daniel Norred <daniel@nor.red>
2021-07-31 10:30:21 -0500

    Other updates

--- 06-tailscale.sh
+++ 06-tailscale.sh
@@ -14,6 +14,7 @@ ip link add br${VLAN}.mac link br${VLAN} type macvlan mode bridge
 ip addr add ${IPV4_GW} dev br${VLAN}.mac noprefixroute
 ip -6 addr add ${IPV6_GW} dev br${VLAN}.mac noprefixroute
 
+
 # set macvlan bridge promiscuous and bring it up
 ip link set br${VLAN}.mac promisc on
 ip link set br${VLAN}.mac up
@@ -22,17 +23,16 @@ ip link set br${VLAN}.mac up
 ip route add ${IPV4_IP}/32 dev br${VLAN}.mac
 ip -6 route add ${IPV6_IP}/128 dev br${VLAN}.mac
 
-# Add Tailscale IP Routes via Container
-ip route add 100.64.0.0/10 via ${IPV4_IP} dev br${VLAN}.mac
 ip -6 route add fd7a:115c:a1e0:ab12:4843::/80 via ${IPV6_IP}
+
+# Add Tailscale IP Routes via Container
+ip route add 100.64.0.0/10 via ${IPV4_IP} dev br${VLAN}.macip -6 route add 2600:1f16:116:3b00::/56 via ${IPV6_IP}
 
 #Add routes from other subnet routers
 ip route add 10.2.0.0/16 via ${IPV4_IP}
-ip -6 route add 2600:1f16:116:3b00::/56 via ${IPV6_IP}
 
 #start container & tailscale 
 docker start tailscaled
 docker exec tailscaled tailscale up --hostname=UDM-Pro --accept-routes=true --advertise-routes=10.1.0.0/16,2600:1700:eec0:7310::/60
-
 
 
